c34a2446b9d1914d1e54aa3ce77000047f4b07e2
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-08-04 11:03:35 +0300

    1.0.3

--- Settings.php
+++ Settings.php
@@ -1,12 +1,19 @@
 <?php
 namespace Dfe\BackendLoginAutocomplete;
+use Magento\Framework\App\ScopeInterface as S;
 /** @method static Settings s() */
 class Settings extends \Df\Core\Settings {
-	/** @return bool */
-	public function enable() {return $this->b('autocomplete');}
+	/**
+	 * @override
+	 * @see \Df\Core\Settings::enable()
+	 * @param null|string|int|S $s [optional]
+	 * @return bool
+	 */
+	public function enable($s = null) {return $this->b('autocomplete', $s);}
 
 	/**
 	 * @override
+	 * @see \Df\Core\Settings::prefix()
 	 * @used-by \Df\Core\Settings::v()
 	 * @return string
 	 */
